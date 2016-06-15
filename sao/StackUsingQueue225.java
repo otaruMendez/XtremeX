ff62ae6e3d59b4a716b3b9ea9f20a26c54ad5e79
Oyewale Ademola <saopayne@gmail.com>
2016-06-15 19:34:00 +0100

    Fix EMpty Stack file

--- sao/StackUsingQueue225.java
+++ sao/StackUsingQueue225.java
@@ -11,4 +11,30 @@ package One;
  */
 public class StackUsingQueue225 {
     
+    LinkedList<Integer> queue = new LinkedList<>();
+
+    // Push element x onto stack.
+    public void push(int x) {
+        queue.add(x);
+        int size = queue.size();
+        while (size > 1) {
+            queue.add(queue.remove());
+            size--;
+        }
+    }
+ 
+    // Removes the element on top of the stack.
+    public void pop() {
+        queue.remove();
+    }
+ 
+    // Get the top element.
+    public int top() {
+        return queue.peek();
+    }
+ 
+    // Return whether the stack is empty.
+    public boolean empty() {
+        return queue.isEmpty();
+    }
 }

4baf1ace48da074759e3d7a35d7debe8a151b013
Oyewale Ademola <saopayne@gmail.com>
2016-06-29 19:11:40 +0100

    Updated source files

--- sao/ContainsDuplicateII219.java
+++ sao/ContainsDuplicateII219.java
@@ -11,4 +11,27 @@ package One;
  */
 public class ContainsDuplicateII219 {
     
-}
+   /*Given an array of integers and an integer k, 
+     * find out whether there are two distinct indices i and j in the array such 
+     * that nums[i] = nums[j] and the difference between i and j is at most k.
+     * @param args 
+     */
+
+    public static void main(String [] args){
+        
+    }
+    
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int len = nums.length;
+        if(k<0) return false;
+        Map <Integer, Integer> keyMap = new HashMap <> ();
+        for (int i = 0; i < nums.length; i++) {
+            if (!keyMap.containsKey (nums[i]) || i - keyMap.get (nums[i]) > k) {
+            } else {
+                return true;
+            }
+            keyMap.put (nums[i], i);
+        }
+        return false;
+    }
+    }
--- sao/PerfectSquares279.java
+++ sao/PerfectSquares279.java
@@ -9,6 +9,22 @@ package One;
  *
  * @author saopayne
  */
-public class PerfectSquares279 {
+public class PerfectSquares279
+
+   public static void main(String [] args){
+        
+    }
+    
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            //set as max
+            dp[i] = i; 
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i - j * j] + 1, dp[i]);
+            }
+        }
+        return dp[n];
+    }
     
 }
